382710e7dd69a6ccfad3209036c5fe87fed376bb
Christian Lara <christianramirezlara23@gmail.com>
2022-03-15 12:43:29 -0600

    Create a ProductModel for the data.

---
+++ src/models.ts
@@ -0,0 +1,25 @@
+interface ImageModel {
+  desktop: string;
+  mobile: string;
+  tablet: string;
+}
+
+export interface ProductModel {
+  category: string;
+  categoryImage: ImageModel;
+  description: string;
+  features: string;
+  gallery: { first: ImageModel; second: ImageModel; third: ImageModel };
+  id: number;
+  image: ImageModel;
+  includes: { quantity: number; item: string }[];
+  name: string;
+  new: boolean;
+  others: {
+    image: { mobile: string; tablet: string; desktop: string };
+    name: string;
+    slug: string;
+  }[];
+  price: number;
+  slug: string;
+}
