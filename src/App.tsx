b627465b1b58e053c39b5a2ecea230fe4796d116
Christian Lara <christianramirezlara23@gmail.com>
2022-03-17 16:35:20 -0600

    Create a Wrapper component for the scroll.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import { Route, Routes } from 'react-router-dom';
+import { useLayoutEffect } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import './App.scss';
 import Category from './layouts/Category/Category';
 import Home from './layouts/Home/Home';
+import Product from './layouts/Product/Product';
+
+const Wrapper = ({ children }: any) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+
+  return children;
+};
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/:category" element={<Category />} />
-    </Routes>
+    <Wrapper>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:category" element={<Category />} />
+        <Route path="/:category/:product" element={<Product />} />
+      </Routes>
+    </Wrapper>
   );
 }
 
