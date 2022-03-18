f4d05ad3998bca0d1f391c243d97c24174a599fb
Christian Lara <christianramirezlara23@gmail.com>
2022-03-18 09:45:54 -0600

    Work in the Cart component.

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,8 +1,24 @@
+import Button from '../Button/Button';
 import Modal from '../Modal/Modal';
 import './Cart.scss';
 
 const Cart = () => {
-  return <Modal>h1</Modal>;
+  return (
+    <Modal>
+      <div className="cart">
+        <div className="cart__header">
+          <h5>CART (3)</h5>
+          <h6>Remove all</h6>
+        </div>
+        <div className="cart__item"></div>
+        <div className="cart__total">
+          <h6>TOTAL</h6>
+          <h5>$5,396</h5>
+        </div>
+        <Button color="orange" textContent="CHECKOUT" />
+      </div>
+    </Modal>
+  );
 };
 
 export default Cart;
