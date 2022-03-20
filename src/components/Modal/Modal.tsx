6a538a600eb5bd4d91d4d0e36a7f014f87c03700
Christian Lara <christianramirezlara23@gmail.com>
2022-03-20 17:56:48 -0600

    Restrict the clicks in the overlay of the Order modal.

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -2,20 +2,13 @@ import './Modal.scss';
 import ReactDOM from 'react-dom';
 import { useDispatch } from 'react-redux';
 import { cartActions } from '../../store/cart';
-import { orderModalActions } from './../../store/orderModal';
 
-const Backdrop = ({ type }: any) => {
+const Backdrop = () => {
   const dispatch = useDispatch();
 
   const closeCartModal = () => dispatch(cartActions.closeCart());
-  const closeOrderModal = () => dispatch(orderModalActions.close());
 
-  return (
-    <div
-      onClick={type === 'cart' ? closeCartModal : closeOrderModal}
-      className="backdrop"
-    />
-  );
+  return <div onClick={closeCartModal} className="backdrop" />;
 };
 
 const ModalOverlay = ({ children, type }: any) => {
@@ -31,7 +24,7 @@ const portalElement = document.getElementById('overlays')!;
 const Modal = ({ children, type }: any) => {
   return (
     <>
-      {ReactDOM.createPortal(<Backdrop type={type} />, portalElement)}
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
       {ReactDOM.createPortal(
         <ModalOverlay type={type}>{children}</ModalOverlay>,
         portalElement
