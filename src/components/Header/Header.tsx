9c066785339d418433d5e72b05c2b8cc41f28e62
Christian Lara <christianramirezlara23@gmail.com>
2022-04-04 08:20:39 -0500

    Make responsive the Header.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,6 +6,7 @@ import Links from '../Links/Links';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { cartActions } from '../../store/cart';
+import { useMediaQuery } from 'react-responsive';
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -13,12 +14,26 @@ const Header = () => {
   const location = useLocation();
   const goBackToHomepage = () => navigate('/');
 
+  const isTablet = useMediaQuery({
+    query: '(max-width: 820px)',
+  });
+
   return (
     <header className="header">
       <div onClick={goBackToHomepage} className="header__logo">
         <img src={logo} alt="Logo" />
       </div>
-      <Links />
+      {isTablet ? (
+        <div className="header__hamburger">
+          <img
+            src="/assets/shared/tablet/icon-hamburger.svg"
+            alt="Hamburger icon"
+          />
+        </div>
+      ) : (
+        <Links />
+      )}
+
       <div className="header__cart-icon">
         <img
           style={location.pathname === '/checkout' ? { display: 'none' } : {}}
