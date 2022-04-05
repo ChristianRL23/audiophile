181538674cf30103bcf78fa0dbe11013874332e6
Christian Lara <christianramirezlara23@gmail.com>
2022-04-05 09:35:35 -0500

    Make responsive the ProductsGrid layout.

--- src/layouts/ProductsGrid/ProductsGrid.tsx
+++ src/layouts/ProductsGrid/ProductsGrid.tsx
@@ -2,6 +2,7 @@ import './ProductsGrid.scss';
 import speaker1 from './speaker-1.png';
 import Button from '../../components/Button/Button';
 import { useNavigate } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
 
 const ProductsGrid = () => {
   const navigate = useNavigate();
@@ -10,10 +11,22 @@ const ProductsGrid = () => {
   const seeProduct2 = () => navigate('/speakers/zx7');
   const seeProduct3 = () => navigate('/earphones/yx1');
 
+  const isMobile = useMediaQuery({
+    query: '(max-width: 415px)',
+  });
+
+  const isMobile2 = useMediaQuery({
+    query: '(max-width: 370px)',
+  });
+
   return (
     <section className="products-grid">
       <div className="products-grid__product--speaker-1">
-        <img src={speaker1} alt="Speaker" />
+        <img
+          /* src={isTablet ? './tablet/image-speaker-zx9.png' : speaker1} */
+          src={speaker1}
+          alt="Speaker"
+        />
         <div className="products-grid__product--speaker-1__text">
           <h4>
             <span>ZX9</span>
@@ -33,11 +46,17 @@ const ProductsGrid = () => {
       </div>
       <div className="products-grid__product--speaker-2">
         <div className="products-grid__product--speaker-2__text">
-          <h5>ZX7 SPEAKER</h5>
+          {isMobile ? (
+            <h5>
+              ZX7<span>SPEAKER</span>
+            </h5>
+          ) : (
+            <h5>ZX7 SPEAKER</h5>
+          )}
           <Button
             onClick={seeProduct2}
             style={{ alignSelf: 'flex-start' }}
-            color="transparent"
+            color={isMobile2 ? 'white' : 'transparent'}
             textContent="SEE PRODUCT"
           />
         </div>
@@ -45,7 +64,14 @@ const ProductsGrid = () => {
       <div className="products-grid__product--earphones-image"></div>
       <div className="products-grid__product--earphones-cta">
         <div className="products-grid__product--earphones-cta__text">
-          <h5>YX1 EARPHONES</h5>
+          {isMobile ? (
+            <h5>
+              YX1 <span>EARPHONES</span>
+            </h5>
+          ) : (
+            <h5>YX1 EARPHONES</h5>
+          )}
+
           <Button
             onClick={seeProduct3}
             style={{ alignSelf: 'flex-start' }}
