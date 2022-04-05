8c02affa64f86ffd27f60b55659b5f61211ef95b
Christian Lara <christianramirezlara23@gmail.com>
2022-04-04 20:50:38 -0500

    Add a hamburger menu.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,5 @@
+import { slide as Burger, Item } from 'burger-menu';
+import 'burger-menu/lib/index.css';
 import './Header.scss';
 import logo from './logo.svg';
 import cartIcon from './icon-cart.svg';
@@ -7,15 +9,25 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { cartActions } from '../../store/cart';
 import { useMediaQuery } from 'react-responsive';
+import { useState } from 'react';
 
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const location = useLocation();
   const goBackToHomepage = () => navigate('/');
+  const [burgerMenuIsOpen, setBurgerMenuIsOpen] = useState(false);
+  const navigateFromLink = (to: string) => {
+    navigate(to);
+    setBurgerMenuIsOpen(false);
+  };
 
   const isTablet = useMediaQuery({
     query: '(max-width: 820px)',
+  });
+
+  const isPhone = useMediaQuery({
+    query: '(max-width: 700px)',
   });
 
   return (
@@ -26,6 +38,7 @@ const Header = () => {
       {isTablet ? (
         <div className="header__hamburger">
           <img
+            onClick={() => setBurgerMenuIsOpen(true)}
             src="/assets/shared/tablet/icon-hamburger.svg"
             alt="Hamburger icon"
           />
@@ -33,7 +46,36 @@ const Header = () => {
       ) : (
         <Links />
       )}
-
+      <Burger
+        left
+        className="burger-menu"
+        isOpen={burgerMenuIsOpen}
+        onClose={() => setBurgerMenuIsOpen(false)}
+        width={isPhone ? 200 : 300}
+      >
+        <>
+          <Item
+            onClick={navigateFromLink.bind(null, '/')}
+            itemKey="home"
+            text="HOME"
+          />
+          <Item
+            onClick={navigateFromLink.bind(null, '/headphones')}
+            itemKey="headphones"
+            text="HEADPHONES"
+          />
+          <Item
+            onClick={navigateFromLink.bind(null, '/speakers')}
+            itemKey="speakers"
+            text="SPEAKERS"
+          />
+          <Item
+            onClick={navigateFromLink.bind(null, '/earphones')}
+            itemKey="earphones"
+            text="EARPHONES"
+          />
+        </>
+      </Burger>
       <div className="header__cart-icon">
         <img
           style={location.pathname === '/checkout' ? { display: 'none' } : {}}
